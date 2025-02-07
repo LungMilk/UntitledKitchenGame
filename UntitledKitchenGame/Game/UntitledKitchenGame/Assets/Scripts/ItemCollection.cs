9ae26f805092e7444540c0e3e87f457002a3f971
LungMilk <joeplace913@gmail.com>
2025-02-07 16:59:10 -0500

    Fixed issue and orders are now generating to a UI element based on the stuff

---
+++ UntitledKitchenGame/Game/UntitledKitchenGame/Assets/Scripts/ItemCollection.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemCollection.Asset", menuName = "Data/Item Collection")]
+public class ItemCollection : ScriptableObject
+{
+    public List<FoodItem> Items;
+
+    //I think I want boolean values of the type to add more customization but rn it works
+    public type onlySelect = new type();
+    //maybe we could have the variables change as well depending on what kind of object it is?
+    public enum type
+    {
+        Unlisted,
+        Meat,
+        Grain,
+        Veggie,
+        Fish
+    }
+    //how can I get this item to automatically collect the items from the files.
+    //[ContextMenu("Select Item")]
+    ////could feed it a colleciton of an item data base but right now let us randomize it.
+    //public FoodItem SelectRandomItem()
+    //{
+    //    //not looking for an index looking for the rarity.
+    //    int randomItem = Random.Range(0, 101);
+    //    Debug.Log(randomItem);
+    //    foreach (FoodItem item in Items)
+    //    {
+    //        //seem to be having an issue with what item is selected.
+    //        //seems to grab the small items first? question about how it operates is required for the veggies as it seemed to miss onions and lettuce.
+    //        if (randomItem <= item.rarity)
+    //        {
+    //            if (item.type.ToString() == onlySelect.ToString())
+    //            {
+    //                //Debug.Log(item.name);
+    //                return item;
+
+    //            }else if (onlySelect == type.Unlisted)
+    //            {
+    //                //Debug.Log(item.name);
+    //                return item;
+    //            }
+    //        }
+    //    }
+    //    return null;
+    //}
+}
---
+++ UntitledKitchenGame/Game/UntitledKitchenGame/Assets/Scripts/OrderGenerator.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class OrderGenerator : MonoBehaviour
+{
+    //this item will return a set amount of orders and we can modify it to be either focused on orders or all items.
+    //for now we will make this able to set the UI elements to their respective texts.
+    public Canvas orderCanvas;
+    public List<TextMeshProUGUI> orderText;
+
+    public ItemCollection itemDatabase;
+
+    public List<FoodItem> foodItems;
+    public int itemMax = 3;
+
+    public type onlySelect = new type();
+    //maybe we could have the variables change as well depending on what kind of object it is?
+    public enum type
+    {
+        Unlisted,
+        Meat,
+        Grain,
+        Veggie,
+        Fish
+    }
+    [ContextMenu("Populate UI")]
+    void PopulateUI()
+    {
+        if (orderCanvas == null || orderText == null) 
+        { 
+        orderCanvas = this.GetComponentInChildren<Canvas>();
+        TextMeshProUGUI[] tempList = this.GetComponentsInChildren<TextMeshProUGUI>();
+
+            for (int i = 0; i < itemMax; i++)
+            {
+                orderText.Add(tempList[i]);
+            }
+        }
+        
+        for(int i = 0; i < orderText.Count; i++)
+        {
+            orderText[i].text = foodItems[i].displayName;
+        }
+    }
+    // Start is called before the first frame update
+    [ContextMenu ("Populate Order")]
+    void PopulateOrder()
+    {
+        //I want if at max to exchange the items for new ones
+        //changing the item max must also cull the list to less items
+        if (foodItems.Count >= itemMax)
+        {
+            foodItems.Clear ();
+        }
+
+        for (int i = 0; i < itemMax; i++)
+        {
+            var selectedItem = SelectRandomItem();
+            
+            foodItems.Add(selectedItem);
+        }
+    }
+    FoodItem SelectRandomItem()
+    {
+        //not looking for an index looking for the rarity.
+        int randomItem = Random.Range(0, 101);
+        //Debug.Log(randomItem);
+        //found the little issue
+        foreach (FoodItem item in itemDatabase.Items)
+        {
+            //seem to be having an issue with what item is selected.
+            //seems to grab the small items first? question about how it operates is required for the veggies as it seemed to miss onions and lettuce.
+
+            //in specific cases items can simply not appear so forcing an item to be returned should be a good moment of functionality but rn we can ignore it.
+            if (randomItem <= item.rarity)
+            {
+                if (item.type.ToString() == onlySelect.ToString())
+                {
+                    //Debug.Log(item.name);
+                    return item;
+
+                }
+                else if (onlySelect == type.Unlisted)
+                {
+                    //Debug.Log(item.name);
+                    return item;
+                }
+            }
+        }
+        return null;
+    }
+}
