b41b5be9ea75396bac83110137581193c497b067
LungMilk <joeplace913@gmail.com>
2025-03-11 16:11:31 -0400

    Fixed the order generator to have null cases if the object does not have a UI
    error of using order generator with the spawner so added cases to remove the errors.

--- UntitledKitchenGame/Game/UntitledKitchenGame/Assets/Scripts/OrderGenerator.cs
+++ UntitledKitchenGame/Game/UntitledKitchenGame/Assets/Scripts/OrderGenerator.cs
@@ -10,12 +10,12 @@ public class OrderGenerator : MonoBehaviour
     //this item will return a set amount of orders and we can modify it to be either focused on orders or all items.
     //for now we will make this able to set the UI elements to their respective texts.
     public Canvas orderCanvas;
-    public List<TextMeshProUGUI> orderText;
-    public List<Image> orderImages;
+    public List<TextMeshProUGUI> orderText = new List<TextMeshProUGUI>();
+    public List<Image> orderImages = new List<Image>();
 
     public ItemCollection itemDatabase;
 
-    public List<FoodItem> foodItems;
+    public List<FoodItem> foodItems = new List<FoodItem>();
     //being a prefab means the object cannot be scene referenced so we gots to find a score manager
     public ScoreManager scoreManage;
     public int itemMax = 3;
@@ -30,97 +30,129 @@ public class OrderGenerator : MonoBehaviour
         Veggie,
         Fish
     }
+
     public void Start()
     {
-        GameObject tempObj;
-        tempObj = GameObject.Find("ScoreManager");
+        GameObject tempObj = GameObject.Find("ScoreManager");
         scoreManage = tempObj.GetComponent<ScoreManager>();
         GenerateOrder();
     }
+
     private void Update()
     {
         PopulateUI();
     }
+
     public void GenerateOrder()
     {
         PopulateUI();
         PopulateOrder();
     }
+
     //no longer needed but why not keep them
     [ContextMenu("Populate UI")]
     void PopulateUI()
     {
-        //need to add null edge cases
-        //or my index is getting out of bounds and so every for loop needs a -1
-        if (orderCanvas == null || orderText == null || orderImages ==null) 
-        { 
-            orderCanvas = this.GetComponentInChildren<Canvas>();
-            Image[] tempImageList = this.GetComponentsInChildren<Image>();
-            for (int i = 0; i < itemMax; i++)
-            {
-                orderImages.Add(tempImageList[i]);
-            }
-            TextMeshProUGUI[] tempList = this.GetComponentsInChildren<TextMeshProUGUI>();
-
-            for (int i = 0; i < itemMax; i++)
-            {
-                orderText.Add(tempList[i]);
-            }
-
-            
-        }
-        
-        for(int i = 0; i < orderText.Count-1; i++)
+        // Adding null checks to avoid errors if the UI elements are not set up
+        if (orderCanvas == null)
         {
-            if (foodItems[i] == null && orderText[i] != null) { orderText[i].gameObject.SetActive(false); } 
-            else { orderText[i].gameObject.SetActive(true); 
-                orderText[i].text = foodItems[i].displayName; }
-            
+            Debug.LogWarning("Order Canvas is not assigned.");
+            return;
         }
-        for (int i = 0; i < orderImages.Count - 1; i++)
+
+        if (orderText == null || orderImages == null)
         {
-            if (foodItems[i] == null) { orderImages[i].gameObject.SetActive(false); }
-            else
+            Debug.LogWarning("UI lists are not assigned.");
+            return;
+        }
+
+        // Adding checks to avoid exceptions if UI components are not present
+        if (orderCanvas != null && orderText != null && orderImages != null)
+        {
+            // Initialize order images and texts if not already set
+            if (orderText.Count == 0 || orderImages.Count == 0)
             {
-                orderImages[i].gameObject.SetActive(true);
-                orderImages[i].sprite = foodItems[i].displayImage;
+                Image[] tempImageList = this.GetComponentsInChildren<Image>();
+                TextMeshProUGUI[] tempList = this.GetComponentsInChildren<TextMeshProUGUI>();
+
+                for (int i = 0; i < Mathf.Min(itemMax, tempImageList.Length); i++)
+                {
+                    if (tempImageList[i] != null)
+                        orderImages.Add(tempImageList[i]);
+                }
+
+                for (int i = 0; i < Mathf.Min(itemMax, tempList.Length); i++)
+                {
+                    if (tempList[i] != null)
+                        orderText.Add(tempList[i]);
+                }
             }
 
+            for (int i = 0; i < orderText.Count; i++)
+            {
+                if (i < foodItems.Count && foodItems[i] != null && orderText[i] != null)
+                {
+                    orderText[i].gameObject.SetActive(true);
+                    orderText[i].text = foodItems[i].displayName;
+                }
+                else if (orderText[i] != null)
+                {
+                    orderText[i].gameObject.SetActive(false);
+                }
+            }
+
+            for (int i = 0; i < orderImages.Count; i++)
+            {
+                if (i < foodItems.Count && foodItems[i] != null && orderImages[i] != null)
+                {
+                    orderImages[i].gameObject.SetActive(true);
+                    orderImages[i].sprite = foodItems[i].displayImage;
+                }
+                else if (orderImages[i] != null)
+                {
+                    orderImages[i].gameObject.SetActive(false);
+                }
+            }
         }
     }
+
+
     // Start is called before the first frame update
-    [ContextMenu ("Populate Order")]
+    [ContextMenu("Populate Order")]
     void PopulateOrder()
     {
-        //I want if at max to exchange the items for new ones
-        //changing the item max must also cull the list to less items
         if (foodItems.Count >= itemMax)
         {
-            foodItems.Clear ();
+            foodItems.Clear();
         }
 
         for (int i = 0; i < itemMax; i++)
         {
             var selectedItem = SelectRandomItem();
-            
-            foodItems.Add(selectedItem);
+            if (selectedItem != null)
+            {
+                foodItems.Add(selectedItem);
+            }
         }
-        var data = new ItemSubmitData()
-        {
-            requiredObject = foodItems[0].name,
-            requiredObject1 = foodItems[1].name,
-            requiredObject2 = foodItems[2].name,
-        };
-        TelemetryLogger.Log(this, "order generated", data);
 
+        if (foodItems.Count >= itemMax)
+        {
+            var data = new ItemSubmitData()
+            {
+                requiredObject = foodItems[0].name,
+                requiredObject1 = foodItems[1].name,
+                requiredObject2 = foodItems[2].name,
+            };
+            TelemetryLogger.Log(this, "order generated", data);
+        }
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        //currently it is not grabbing the objects collider
-        //Debug.Log(other.name);
         CheckItemSubmission(other.gameObject);
     }
-    //please convert the object so it is visible from teh save file.
+
+    //please convert the object so it is visible from the save file.
     [System.Serializable]
     public struct ItemSubmitData
     {
@@ -129,56 +161,47 @@ public class OrderGenerator : MonoBehaviour
         public string requiredObject2;
         public string requiredObject;
     }
-    public void CheckItemSubmission(GameObject recievedObject)
+
+    public void CheckItemSubmission(GameObject receivedObject)
     {
-        //TelemetryLogger.Log(this, "Item Submitted");
-        //arugably we could take
-        foreach(FoodItem item in foodItems)
+        foreach (FoodItem item in foodItems)
         {
-            if(item.foodObject = recievedObject)
+            if (item.foodObject == receivedObject) // Fixed = to == here
             {
                 var data = new ItemSubmitData()
                 {
-                    objectName = recievedObject.name
+                    objectName = receivedObject.name
                 };
                 TelemetryLogger.Log(this, "Correct Submission", data);
                 scoreManage.score += item.pointValue;
                 foodItems.Remove(item);
+                break; // Item found, break out of the loop
             }
             else
             {
                 var data = new ItemSubmitData()
                 {
-                    objectName = recievedObject.name
+                    objectName = receivedObject.name
                 };
                 TelemetryLogger.Log(this, "Incorrect Submission", data);
             }
         }
-        
     }
+
     public FoodItem SelectRandomItem()
     {
-        //not looking for an index looking for the rarity.
         int randomItem = Random.Range(0, 101);
-        //Debug.Log(randomItem);
-        //found the little issue
+
         foreach (FoodItem item in itemDatabase.Items)
         {
-            //seem to be having an issue with what item is selected.
-            //seems to grab the small items first? question about how it operates is required for the veggies as it seemed to miss onions and lettuce.
-
-            //in specific cases items can simply not appear so forcing an item to be returned should be a good moment of functionality but rn we can ignore it.
             if (randomItem <= item.rarity)
             {
                 if (item.type.ToString() == onlySelect.ToString())
                 {
-                    //Debug.Log(item.name);
                     return item;
-
                 }
                 else if (onlySelect == type.Unlisted)
                 {
-                    //Debug.Log(item.name);
                     return item;
                 }
             }
@@ -186,3 +209,4 @@ public class OrderGenerator : MonoBehaviour
         return null;
     }
 }
+
