77f10f25b5b78b94c52334ae5af0114a03961909
Adithya-K47 <karthiad@sheridancollege.ca>
2025-03-24 14:42:03 -0400

    Audio added, score UI modified

--- UntitledKitchenGame/Game/UntitledKitchenGame/Assets/Scripts/ScoreManager.cs
+++ UntitledKitchenGame/Game/UntitledKitchenGame/Assets/Scripts/ScoreManager.cs
@@ -8,19 +8,44 @@ public class ScoreManager : MonoBehaviour
     //score manager just has to store and be referenced by anything that wants to increase the score
     //delegate function??? but lets keep it strict object reference.
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI floatingScoreText;
     public EventManager eventManager;
     public float score;
     public float quota;
+    public void AddScore(float points)
+    {
+        StartCoroutine(ShowFloatingScore(points));
+    }
 
     private void Start()
     {
         score = 0;
         eventManager = this.GetComponent<EventManager>();
         scoreText.text = $"Score: {score}/{quota}";
+        floatingScoreText.gameObject.SetActive(false);
     }
     private void Update()
     {
-        scoreText.text = $"Score: {score}/{quota}";
-        if (score >quota ) { eventManager.EndGame(); }
+        //scoreText.text = $"Score: {score}/{quota}";
+        //if (score >quota ) { eventManager.EndGame(); }
+    }
+    private IEnumerator ShowFloatingScore(float points)
+    {
+        floatingScoreText.text = $"+{points}";
+        floatingScoreText.gameObject.SetActive(true);
+
+       
+        yield return new WaitForSeconds(4f);
+
+       
+        floatingScoreText.gameObject.SetActive(false);
+        score += points;
+        scoreText.text = $"{score}";
+
+       
+        if (score > quota)
+        {
+            eventManager.EndGame();
+        }
     }
 }
